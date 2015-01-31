34a17b6785846c844e87b40f3db50c0b3326da0f
Jared Rader <raderj89@gmail.com>
2015-01-31 15:44:15 -0800

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,39 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    return [NSNumber numberWithInt:([number intValue] * 2)];
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *numArray = [@[] mutableCopy];
+    
+    for(NSInteger num = number; num <= otherNumber; num++) {
+        [numArray addObject:([NSNumber numberWithInt:num])];
+    }
+    return numArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSMutableArray *numArray = [arrayOfNumbers mutableCopy];
+    
+    [numArray sortUsingComparator:^NSComparisonResult(id obj1, id obj2){
+        NSNumber *numberA = (NSNumber *)obj1;
+        NSNumber *numberB = (NSNumber *)obj2;
+        
+        int intValueA = [numberA intValue];
+        int intValueB = [numberB intValue];
+        
+        if (intValueA > intValueB) {
+            return NSOrderedDescending;
+        } else if (intValueA < intValueB) {
+            return NSOrderedAscending;
+        }
+        return NSOrderedSame;
+    }];
+    
+    return [numArray[0] longValue];
 }
 
 @end
